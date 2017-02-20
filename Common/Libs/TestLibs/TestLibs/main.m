2d22c072db31ec589e43d587fb32bde91aaa5916
zt-2010879 <zt-2010879@macmini-dfc59e.ztgame.com>
2017-02-20 13:30:10 +0800

    add project file

---
+++ Common/Libs/TestLibs/TestLibs/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestLibs
+//
+//  Created by zt-2010879 on 2017/2/20.
+//  Copyright © 2017年 zt-2010879. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
