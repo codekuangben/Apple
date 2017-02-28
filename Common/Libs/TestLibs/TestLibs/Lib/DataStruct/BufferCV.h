c44dd568e0a96b0634923fc413bc03c0d976f8ab
codekuangben <kuangben2001@163.com>
2017-02-28 22:38:31 +0800

    aa

---
+++ Common/Libs/TestLibs/TestLibs/Lib/DataStruct/BufferCV.h
@@ -0,0 +1,8 @@
+﻿#ifndef __BufferCV_h
+#define __BufferCV_h
+
+const int INIT_ELEM_CAPACITY = 32;          // 默认分配 32 元素
+const int INIT_CAPACITY = 1 * 1024;         // 默认分配 1 K
+const int MAX_CAPACITY = 8 * 1024 * 1024;   // 最大允许分配 8 M
+
+#endif
