4405442ab3e790d33862900de7a29ce3ef1323a9
codekuangben <kuangben2001@163.com>
2017-02-28 23:15:17 +0800

    aaa

---
+++ Common/Libs/TestLibs/TestLibs/Lib/DelayHandle/IDelayHandleItem.h
@@ -0,0 +1,12 @@
+﻿#ifndef __IDelayHandleItem_h
+#define __IDelayHandleItem_h
+
+/**
+ * @brief 延迟添加的对象
+ */
+@protocol IDelayHandleItem <NSObject>
+    - (void) setClientDispose: (bool) isDispose;
+    - (bool) isClientDispose;
+@end
+
+#endif
