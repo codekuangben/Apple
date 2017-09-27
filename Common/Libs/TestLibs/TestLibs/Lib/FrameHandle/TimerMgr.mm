70fdb83102f1c729b88ebe9cba94c9ccd9056299
lvguanghai <kuangben2001@163.com>
2017-09-27 10:11:22 +0800

    修改

--- Common/Libs/TestLibs/TestLibs/Lib/FrameHandle/TimerMgr.mm
+++ Common/Libs/TestLibs/TestLibs/Lib/FrameHandle/TimerMgr.mm
@@ -1,116 +1,107 @@
-﻿/**
-* @brief 定时器管理器
-*/
-package SDK.Lib.FrameHandle;
+﻿#ifndef __TimerMgr_H
+#define __TimerMgr_H
 
-import SDK.Lib.DataStruct.MList;
-import SDK.Lib.DelayHandle.DelayHandleMgrBase;
-import SDK.Lib.DelayHandle.IDelayHandleItem;
-import SDK.Lib.Tools.UtilApi;
+@implementation TimerMgr
 
-public class TimerMgr extends DelayHandleMgrBase
+-(void) init()
 {
-    protected MList<TimerItemBase> mTimerList;     // 当前所有的定时器列表
-
-    public TimerMgr()
-    {
-        self.mTimerList = new MList<TimerItemBase>();
-    }
-
-    @Override
-    public (void) init()
-    {
-
-    }
-
-    @Override
-    public (void) dispose()
-    {
-
-    }
-
-    @Override
-    protected (void) addObject(IDelayHandleItem delayObject)
-    {
-        self.addObject(delayObject, 0);
-    }
-
-    @Override
-    protected (void) addObject(IDelayHandleItem delayObject, float priority)
-    {
-        // 检查当前是否已经在队列中
-        if (!self.mTimerList.Contains((TimerItemBase)delayObject))
-        {
-            if (self.mLoopDepth.isInDepth())
-            {
-                super.addObject(delayObject, priority);
-            }
-            else
-            {
-                self.mTimerList.Add((TimerItemBase)delayObject);
-            }
-        }
-    }
-
-    @Override
-    protected (void) removeObject(IDelayHandleItem delayObject)
-    {
-        // 检查当前是否在队列中
-        if (self.mTimerList.Contains((TimerItemBase)delayObject))
-        {
-            ((TimerItemBase)delayObject).mDisposed = true;
-
-            if (self.mLoopDepth.isInDepth())
-            {
-                super.removeObject(delayObject);
-            }
-            else
-            {
-                for(TimerItemBase item : self.mTimerList.list())
-                {
-                    if (UtilApi.isAddressEqual(item, delayObject))
-                    {
-                        self.mTimerList.Remove(item);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    public (void) addTimer(TimerItemBase delayObject)
-    {
-        self.addTimer(delayObject, 0);
-    }
-
-    // 从 Lua 中添加定时器，这种定时器尽量整个定时器周期只与 Lua 通信一次
-    public (void) addTimer(TimerItemBase delayObject, float priority)
-    {
-        self.addObject(delayObject, priority);
-    }
-
-    public (void) removeTimer(TimerItemBase timer)
-    {
-        self.removeObject(timer);
-    }
-
-    public (void) Advance(float delta)
-    {
-        self.mLoopDepth.incDepth();
-
-        for(TimerItemBase timerItem : self.mTimerList.list())
-        {
-            if (!timerItem.isClientDispose())
-            {
-                timerItem.OnTimer(delta);
-            }
-
-            if (timerItem.mDisposed)        // 如果已经结束
-            {
-                self.removeObject(timerItem);
-            }
-        }
-
-        self.mLoopDepth.decDepth();
-    }
+	if(self = [super init])
+	{
+		self.mTimerList = [[MList alloc] init];
+	}
+	
+	return self;
 }
+
+@Override
+public (void) dispose()
+{
+
+}
+
+@Override
+protected (void) addObject(IDelayHandleItem delayObject)
+{
+	self.addObject(delayObject, 0);
+}
+
+@Override
+protected (void) addObject(IDelayHandleItem delayObject, float priority)
+{
+	// 检查当前是否已经在队列中
+	if (!self.mTimerList.Contains((TimerItemBase)delayObject))
+	{
+		if (self.mLoopDepth.isInDepth())
+		{
+			super.addObject(delayObject, priority);
+		}
+		else
+		{
+			self.mTimerList.Add((TimerItemBase)delayObject);
+		}
+	}
+}
+
+@Override
+protected (void) removeObject(IDelayHandleItem delayObject)
+{
+	// 检查当前是否在队列中
+	if (self.mTimerList.Contains((TimerItemBase)delayObject))
+	{
+		((TimerItemBase)delayObject).mDisposed = true;
+
+		if (self.mLoopDepth.isInDepth())
+		{
+			super.removeObject(delayObject);
+		}
+		else
+		{
+			for(TimerItemBase item : self.mTimerList.list())
+			{
+				if (UtilApi.isAddressEqual(item, delayObject))
+				{
+					self.mTimerList.Remove(item);
+					break;
+				}
+			}
+		}
+	}
+}
+
+public (void) addTimer(TimerItemBase delayObject)
+{
+	self.addTimer(delayObject, 0);
+}
+
+// 从 Lua 中添加定时器，这种定时器尽量整个定时器周期只与 Lua 通信一次
+public (void) addTimer(TimerItemBase delayObject, float priority)
+{
+	self.addObject(delayObject, priority);
+}
+
+public (void) removeTimer(TimerItemBase timer)
+{
+	self.removeObject(timer);
+}
+
+public (void) Advance(float delta)
+{
+	self.mLoopDepth.incDepth();
+
+	for(TimerItemBase timerItem : self.mTimerList.list())
+	{
+		if (!timerItem.isClientDispose())
+		{
+			timerItem.OnTimer(delta);
+		}
+
+		if (timerItem.mDisposed)        // 如果已经结束
+		{
+			self.removeObject(timerItem);
+		}
+	}
+
+	self.mLoopDepth.decDepth();
+}
+
+#endif
--- Common/Libs/TestLibs/TestLibs/Lib/FrameHandle/TimerType.h
+++ Common/Libs/TestLibs/TestLibs/Lib/FrameHandle/TimerType.h
@@ -1,12 +1,15 @@
-﻿/**
+﻿#ifndef __TimerType_H
+#define __TimerType_H
+
+/**
 * @brief 定时器类型
 */
-package SDK.Lib.FrameHandle;
-
-public enum TimerType
+typedef enum
 {
     eTickTimer,             // 每一帧定时器
     eOneSecTimer,           // 1 秒定时器
     eFiveSecTimer,          // 5 秒定时器
     eTimerTotla             // 总共定时器种类
-}
+} TimerType;
+
+#endif
