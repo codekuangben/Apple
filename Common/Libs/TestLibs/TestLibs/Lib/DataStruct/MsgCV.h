c5e904762ed5b62d53df68f2bf55207ae7e20d80
codekuangben <guanghai.lv@mihoyo.com>
2020-09-28 13:47:05 +0800

    change

--- Common/Libs/TestLibs/TestLibs/Lib/DataStruct/MsgCV.h
+++ Common/Libs/TestLibs/TestLibs/Lib/DataStruct/MsgCV.h
@@ -2,7 +2,11 @@
 
 @interface MsgCV
 {
-    public static final (int) PACKET_ZIP_MIN = 32;
-    public static final (int) PACKET_ZIP = 0x40000000;
-    public static final (int) HEADER_SIZE = 4;   // 包长度占据几个字节
+
 }
+
++ (int) PACKET_ZIP_MIN;
++ (int) PACKET_ZIP;
++ (int) HEADER_SIZE;
+
+@end
