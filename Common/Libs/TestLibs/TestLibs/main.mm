87c338e07acaeece168f52ee95e4c2d7d8acd8c0
guanghai.lv <guanghai.lv@guanghailvdeMac-mini.local>
2020-10-01 05:59:59 +0800

    change

--- Common/Libs/TestLibs/TestLibs/main.mm
+++ Common/Libs/TestLibs/TestLibs/main.mm
@@ -5,14 +5,15 @@
 #import <Foundation/Foundation.h>
 #import "TestA.h"
 
-(int) main((int) argc, const char * argv[]) {
+int main(int argc, const char * argv[])
+{
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
         
         TestA* testA = [[TestA alloc] init];
-        [testA setA: 10	];
-        [testA setAll: 10	paramStr: @"name"];
+        [testA setA: 10];
+        [testA setAll: 10 paramStr: @"name"];
         
         [TestA setB: 20];
     }
