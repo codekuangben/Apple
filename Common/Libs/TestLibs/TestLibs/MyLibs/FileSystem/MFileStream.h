d8d846ebd9a6d8ffb0e3ad380db48aacc4b591f3
codekuangben <kuangben2001@163.com>
2020-10-03 21:11:43 +0800

    change

--- Common/Libs/TestLibs/TestLibs/MyLibs/FileSystem/MFileStream.h
+++ Common/Libs/TestLibs/TestLibs/MyLibs/FileSystem/MFileStream.h
@@ -1,223 +1,223 @@
-#import "MyLibs/Base/GObject.h"
-#import "MyLibs/EventHandle/IDispatchObject.h"
+// #import "MyLibs/Base/GObject.h"
+// #import "MyLibs/EventHandle/IDispatchObject.h"
 
-/**
- * @brief 仅支持本地文件操作，仅支持同步操作
- */
-@interface MFileStream : GObject <IDispatchObject>
-{
-@public 
-	File mFile;
-    FileInputStream mFileInputStream;
-    FileOutputStream mFileOutputStream;
+// /**
+//  * @brief 仅支持本地文件操作，仅支持同步操作
+//  */
+// @interface MFileStream : GObject <IDispatchObject>
+// {
+// @public 
+// 	File mFile;
+//     FileInputStream mFileInputStream;
+//     FileOutputStream mFileOutputStream;
 
-@protected
-	NSString* mFilePath;
-    FileOpState mFileOpState;
+// @protected
+// 	NSString* mFilePath;
+//     FileOpState mFileOpState;
 
-    NSString* mText;
-    char[] mBytes;
-}
+//     NSString* mText;
+//     char[] mBytes;
+// }
 
-/*
-@property() File mFile;
-@property() FileInputStream mFileInputStream;
-@property() FileOutputStream mFileOutputStream;
-@property() NSString* mFilePath;
+// /*
+// @property() File mFile;
+// @property() FileInputStream mFileInputStream;
+// @property() FileOutputStream mFileOutputStream;
+// @property() NSString* mFilePath;
 
-@property() FileOpState mFileOpState;
-@property() NSString* mText;
-@property() char[] mBytes;
-*/
+// @property() FileOpState mFileOpState;
+// @property() NSString* mText;
+// @property() char[] mBytes;
+// */
 
--(id) init:(NSString*) filePath
-{
-	if(self = [super init])
-	{
-		self->mTypeId = "MFileStream";
+// -(id) init:(NSString*) filePath
+// {
+// 	if(self = [super init])
+// 	{
+// 		self->mTypeId = "MFileStream";
 
-		self->mFilePath = filePath;
-		self->mFileOpState = FileOpState.eNoOp;
+// 		self->mFilePath = filePath;
+// 		self->mFileOpState = FileOpState.eNoOp;
 
-		[self checkAndOpen];
-	}
-}
+// 		[self checkAndOpen];
+// 	}
+// }
 
--(void) seek:(long: offset origin:(MSeekOrigin) origin
-{
-	if(self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if(MSeekOrigin.Begin == origin)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(offset);
-				}
+// -(void) seek:(long: offset origin:(MSeekOrigin) origin
+// {
+// 	if(self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if(MSeekOrigin.Begin == origin)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-		else if(MSeekOrigin.End == origin)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(self->mFile.length() - 1 - offset);
-				}
+// 			}
+// 		}
+// 		else if(MSeekOrigin.End == origin)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(self->mFile.length() - 1 - offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(self->mFile.length() - 1 - offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(self->mFile.length() - 1 - offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-		else
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(self->mFileInputStream.getChannel().position() + offset);
-				}
+// 			}
+// 		}
+// 		else
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(self->mFileInputStream.getChannel().position() + offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(self->mFileOutputStream.getChannel().position() + offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(self->mFileOutputStream.getChannel().position() + offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-	}
-}
+// 			}
+// 		}
+// 	}
+// }
 
--(void) dispose
-{
-	[self close];
-}
+// -(void) dispose
+// {
+// 	[self close];
+// }
 
--(void) checkAndOpen
-{
-	if(self->mFileOpState == FileOpState.eNoOp)
-	{
-		self->mFile = new File(self->mFilePath);
-	}
-}
+// -(void) checkAndOpen
+// {
+// 	if(self->mFileOpState == FileOpState.eNoOp)
+// 	{
+// 		self->mFile = new File(self->mFilePath);
+// 	}
+// }
 
--(void) syncOpenFileStream
-{
-	if (self->mFileOpState == FileOpState.eNoOp)
-	{
-		self->mFileOpState = FileOpState.eOpening;
+// -(void) syncOpenFileStream
+// {
+// 	if (self->mFileOpState == FileOpState.eNoOp)
+// 	{
+// 		self->mFileOpState = FileOpState.eOpening;
 
-		try
-		{
-			self->mFile = new File(mFilePath);
-			self->mFileOpState = FileOpState.eOpenSuccess;
-		}
-		catch(Exception exp)
-		{
-			self->mFileOpState = FileOpState.eOpenFail;
-		}
-	}
-}
+// 		try
+// 		{
+// 			self->mFile = new File(mFilePath);
+// 			self->mFileOpState = FileOpState.eOpenSuccess;
+// 		}
+// 		catch(Exception exp)
+// 		{
+// 			self->mFileOpState = FileOpState.eOpenFail;
+// 		}
+// 	}
+// }
 
--(BOOL) isValid
-{
-	return self->mFileOpState == FileOpState.eOpenSuccess;
-}
+// -(BOOL) isValid
+// {
+// 	return self->mFileOpState == FileOpState.eOpenSuccess;
+// }
 
-// 获取总共长度
--(int) getLength
-{
-	(int) len = 0;
+// // 获取总共长度
+// -(int) getLength
+// {
+// 	(int) len = 0;
 
-	if (self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if (self->mFile != nil)
-		{
-			len = ((int))self->mFile.length();
-		}
-		/*
-		if (mFileStream != nil && mFileStream.CanSeek)
-		{
-			try
-			{
-				len = ((int))mFileStream.Seek(0, SeekOrigin.End);     // 移动到文件结束，返回长度
-				len = ((int))mFileStream.Position;                    // Position 移动到 Seek 位置
-			}
-			catch(Exception exp)
-			{
-			}
-		}
-		*/
-	}
+// 	if (self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if (self->mFile != nil)
+// 		{
+// 			len = ((int))self->mFile.length();
+// 		}
+// 		/*
+// 		if (mFileStream != nil && mFileStream.CanSeek)
+// 		{
+// 			try
+// 			{
+// 				len = ((int))mFileStream.Seek(0, SeekOrigin.End);     // 移动到文件结束，返回长度
+// 				len = ((int))mFileStream.Position;                    // Position 移动到 Seek 位置
+// 			}
+// 			catch(Exception exp)
+// 			{
+// 			}
+// 		}
+// 		*/
+// 	}
 
-	return len;
-}
+// 	return len;
+// }
 
--(void) close
-{
-	if (self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if (self->mFile != nil)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.close();
-					self->mFileInputStream = nil;
-				}
+// -(void) close
+// {
+// 	if (self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if (self->mFile != nil)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.close();
+// 					self->mFileInputStream = nil;
+// 				}
 
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileOutputStream.flush();
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileOutputStream.flush();
 
-					self->mFileOutputStream.close();
-					self->mFileOutputStream = nil;
-				}
-			}
-			catch(Exception e)
-			{
+// 					self->mFileOutputStream.close();
+// 					self->mFileOutputStream = nil;
+// 				}
+// 			}
+// 			catch(Exception e)
+// 			{
 
-			}
-		}
+// 			}
+// 		}
 
-		self->mFileOpState = FileOpState.eOpenClose;
-		self->mFileOpState = FileOpState.eNoOp;
-	}
-}
+// 		self->mFileOpState = FileOpState.eOpenClose;
+// 		self->mFileOpState = FileOpState.eNoOp;
+// 	}
+// }
 
--(NSString*) readText;
--(NSString*) readText:(int) offset;
--(NSString*) readText:(int) offset count:(int) count;
--(NSString*) readText:(int) offset count:(int) count encode:(MEncoding) encode;
--(char[]) readByte;
--(char[]) readByte:(int) offset;
--(char[]) readByte:(int) offset count:(int) count;
--(void) writeText:(NSString*) text;
--(void) writeText:(NSString*) text gkEncode:(GkEncode) gkEncode;
-- (void) writeByte(char[] bytes;
-- (void) writeByte(char[] bytes, (int) offset;
-- (void) writeByte(char[] bytes, (int) offset, (int) count;
--(void) writeLine:(NSString*) text;
--(void) writeLine:(NSString*) text gkEncode:(GkEncode) gkEncode;
+// -(NSString*) readText;
+// -(NSString*) readText:(int) offset;
+// -(NSString*) readText:(int) offset count:(int) count;
+// -(NSString*) readText:(int) offset count:(int) count encode:(MEncoding) encode;
+// -(char[]) readByte;
+// -(char[]) readByte:(int) offset;
+// -(char[]) readByte:(int) offset count:(int) count;
+// -(void) writeText:(NSString*) text;
+// -(void) writeText:(NSString*) text gkEncode:(GkEncode) gkEncode;
+// - (void) writeByte(char[] bytes;
+// - (void) writeByte(char[] bytes, (int) offset;
+// - (void) writeByte(char[] bytes, (int) offset, (int) count;
+// -(void) writeLine:(NSString*) text;
+// -(void) writeLine:(NSString*) text gkEncode:(GkEncode) gkEncode;
 
-@end
+// @end
--- Common/Libs/TestLibs/TestLibs/MyLibs/FileSystem/MFileStream.mm
+++ Common/Libs/TestLibs/TestLibs/MyLibs/FileSystem/MFileStream.mm
@@ -1,353 +1,353 @@
-﻿#import "MyLibs/FileSystem/MFileStream.h"
+﻿// #import "MyLibs/FileSystem/MFileStream.h"
 
-@implementation MFileStream
+// @implementation MFileStream
 
--(id) init:(NSString*) filePath
-{
-	if(self = [super init])
-	{
-		self->mTypeId = "MFileStream";
+// -(id) init:(NSString*) filePath
+// {
+// 	if(self = [super init])
+// 	{
+// 		self->mTypeId = "MFileStream";
 
-		self->mFilePath = filePath;
-		self->mFileOpState = FileOpState.eNoOp;
+// 		self->mFilePath = filePath;
+// 		self->mFileOpState = FileOpState.eNoOp;
 
-		[self checkAndOpen];
-	}
-}
+// 		[self checkAndOpen];
+// 	}
+// }
 
--(void) seek:(long) offset origin:(MSeekOrigin) origin
-{
-	if(self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if(MSeekOrigin.Begin == origin)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(offset);
-				}
+// -(void) seek:(long) offset origin:(MSeekOrigin) origin
+// {
+// 	if(self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if(MSeekOrigin.Begin == origin)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-		else if(MSeekOrigin.End == origin)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(self->mFile.length() - 1 - offset);
-				}
+// 			}
+// 		}
+// 		else if(MSeekOrigin.End == origin)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(self->mFile.length() - 1 - offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(self->mFile.length() - 1 - offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(self->mFile.length() - 1 - offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-		else
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.getChannel().position(self->mFileInputStream.getChannel().position() + offset);
-				}
+// 			}
+// 		}
+// 		else
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.getChannel().position(self->mFileInputStream.getChannel().position() + offset);
+// 				}
 
-				if(nil != self->mFileOutputStream)
-				{
-					self->mFileOutputStream.getChannel().position(self->mFileOutputStream.getChannel().position() + offset);
-				}
-			}
-			catch (Exception e)
-			{
+// 				if(nil != self->mFileOutputStream)
+// 				{
+// 					self->mFileOutputStream.getChannel().position(self->mFileOutputStream.getChannel().position() + offset);
+// 				}
+// 			}
+// 			catch (Exception e)
+// 			{
 
-			}
-		}
-	}
-}
+// 			}
+// 		}
+// 	}
+// }
 
--(void) dispose
-{
-	[self close];
-}
+// -(void) dispose
+// {
+// 	[self close];
+// }
 
--(void) checkAndOpen
-{
-	if(self->mFileOpState == FileOpState.eNoOp)
-	{
-		self->mFile = new File(self->mFilePath);
-	}
-}
+// -(void) checkAndOpen
+// {
+// 	if(self->mFileOpState == FileOpState.eNoOp)
+// 	{
+// 		self->mFile = new File(self->mFilePath);
+// 	}
+// }
 
--(void) syncOpenFileStream
-{
-	if (self->mFileOpState == FileOpState.eNoOp)
-	{
-		self->mFileOpState = FileOpState.eOpening;
+// -(void) syncOpenFileStream
+// {
+// 	if (self->mFileOpState == FileOpState.eNoOp)
+// 	{
+// 		self->mFileOpState = FileOpState.eOpening;
 
-		try
-		{
-			self->mFile = new File(mFilePath);
-			self->mFileOpState = FileOpState.eOpenSuccess;
-		}
-		catch(Exception exp)
-		{
-			self->mFileOpState = FileOpState.eOpenFail;
-		}
-	}
-}
+// 		try
+// 		{
+// 			self->mFile = new File(mFilePath);
+// 			self->mFileOpState = FileOpState.eOpenSuccess;
+// 		}
+// 		catch(Exception exp)
+// 		{
+// 			self->mFileOpState = FileOpState.eOpenFail;
+// 		}
+// 	}
+// }
 
--(BOOL) isValid
-{
-	return self->mFileOpState == FileOpState.eOpenSuccess;
-}
+// -(BOOL) isValid
+// {
+// 	return self->mFileOpState == FileOpState.eOpenSuccess;
+// }
 
-// 获取总共长度
--(int) getLength
-{
-	(int) len = 0;
+// // 获取总共长度
+// -(int) getLength
+// {
+// 	(int) len = 0;
 
-	if (self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if (self->mFile != nil)
-		{
-			len = ((int))self->mFile.length();
-		}
-		/*
-		if (mFileStream != nil && mFileStream.CanSeek)
-		{
-			try
-			{
-				len = ((int))mFileStream.Seek(0, SeekOrigin.End);     // 移动到文件结束，返回长度
-				len = ((int))mFileStream.Position;                    // Position 移动到 Seek 位置
-			}
-			catch(Exception exp)
-			{
-			}
-		}
-		*/
-	}
+// 	if (self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if (self->mFile != nil)
+// 		{
+// 			len = ((int))self->mFile.length();
+// 		}
+// 		/*
+// 		if (mFileStream != nil && mFileStream.CanSeek)
+// 		{
+// 			try
+// 			{
+// 				len = ((int))mFileStream.Seek(0, SeekOrigin.End);     // 移动到文件结束，返回长度
+// 				len = ((int))mFileStream.Position;                    // Position 移动到 Seek 位置
+// 			}
+// 			catch(Exception exp)
+// 			{
+// 			}
+// 		}
+// 		*/
+// 	}
 
-	return len;
-}
+// 	return len;
+// }
 
--(void) close
-{
-	if (self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if (self->mFile != nil)
-		{
-			try
-			{
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileInputStream.close();
-					self->mFileInputStream = nil;
-				}
+// -(void) close
+// {
+// 	if (self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if (self->mFile != nil)
+// 		{
+// 			try
+// 			{
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileInputStream.close();
+// 					self->mFileInputStream = nil;
+// 				}
 
-				if(nil != self->mFileInputStream)
-				{
-					self->mFileOutputStream.flush();
+// 				if(nil != self->mFileInputStream)
+// 				{
+// 					self->mFileOutputStream.flush();
 
-					self->mFileOutputStream.close();
-					self->mFileOutputStream = nil;
-				}
-			}
-			catch(Exception e)
-			{
+// 					self->mFileOutputStream.close();
+// 					self->mFileOutputStream = nil;
+// 				}
+// 			}
+// 			catch(Exception e)
+// 			{
 
-			}
-		}
+// 			}
+// 		}
 
-		self->mFileOpState = FileOpState.eOpenClose;
-		self->mFileOpState = FileOpState.eNoOp;
-	}
-}
+// 		self->mFileOpState = FileOpState.eOpenClose;
+// 		self->mFileOpState = FileOpState.eNoOp;
+// 	}
+// }
 
--(NSString*) readText
-{
-	return self->readText(0, 0, nil);
-}
+// -(NSString*) readText
+// {
+// 	return self->readText(0, 0, nil);
+// }
 
--(NSString*) readText:(int) offset
-{
-	return self->readText(offset, 0, nil);
-}
+// -(NSString*) readText:(int) offset
+// {
+// 	return self->readText(offset, 0, nil);
+// }
 
--(NSString*) readText:(int) offset count:(int) count
-{
-	return self->readText(offset, count, nil);
-}
+// -(NSString*) readText:(int) offset count:(int) count
+// {
+// 	return self->readText(offset, count, nil);
+// }
 
--(NSString*) readText:(int) offset count:(int) count encode:(MEncoding) encode
-{
-	self->checkAndOpen();
+// -(NSString*) readText:(int) offset count:(int) count encode:(MEncoding) encode
+// {
+// 	self->checkAndOpen();
 
-	NSString* retStr = "";
-	char[] bytes = nil;
+// 	NSString* retStr = "";
+// 	char[] bytes = nil;
 
-	if (encode == nil)
-	{
-		encode = MEncoding.UTF8;
-	}
+// 	if (encode == nil)
+// 	{
+// 		encode = MEncoding.UTF8;
+// 	}
 
-	if (count == 0)
-	{
-		count = getLength();
-	}
+// 	if (count == 0)
+// 	{
+// 		count = getLength();
+// 	}
 
-	if (self->mFileOpState == FileOpState.eOpenSuccess)
-	{
-		if (self->mFile.canRead())
-		{
-			try
-			{
-				bytes = new char[count];
-				self->mFileInputStream.read(bytes, 0, count);
+// 	if (self->mFileOpState == FileOpState.eOpenSuccess)
+// 	{
+// 		if (self->mFile.canRead())
+// 		{
+// 			try
+// 			{
+// 				bytes = new char[count];
+// 				self->mFileInputStream.read(bytes, 0, count);
 
-				retStr = encode.GetString(bytes);
-			}
-			catch (Exception err)
-			{
+// 				retStr = encode.GetString(bytes);
+// 			}
+// 			catch (Exception err)
+// 			{
 
-			}
-		}
-	}
+// 			}
+// 		}
+// 	}
 
-	return retStr;
-}
+// 	return retStr;
+// }
 
--(char[]) readByte
-{
-	return self->readByte(0, 0);
-}
+// -(char[]) readByte
+// {
+// 	return self->readByte(0, 0);
+// }
 
--(char[]) readByte:(int) offset
-{
-	return self->readByte(offset, 0);
-}
+// -(char[]) readByte:(int) offset
+// {
+// 	return self->readByte(offset, 0);
+// }
 
--(char[]) readByte:(int) offset count:(int) count
-{
-	self->checkAndOpen();
+// -(char[]) readByte:(int) offset count:(int) count
+// {
+// 	self->checkAndOpen();
 
-	if (count == 0)
-	{
-		count = getLength();
-	}
+// 	if (count == 0)
+// 	{
+// 		count = getLength();
+// 	}
 
-	char[] bytes = nil;
+// 	char[] bytes = nil;
 
-	if (self->mFile.canRead())
-	{
-		try
-		{
-			bytes = new char[count];
-			self->mFileInputStream.read(bytes, 0, count);
-		}
-		catch (Exception err)
-		{
+// 	if (self->mFile.canRead())
+// 	{
+// 		try
+// 		{
+// 			bytes = new char[count];
+// 			self->mFileInputStream.read(bytes, 0, count);
+// 		}
+// 		catch (Exception err)
+// 		{
 
-		}
-	}
+// 		}
+// 	}
 
-	return bytes;
-}
+// 	return bytes;
+// }
 
--(void) writeText:(NSString*) text
-{
-	self->writeText(text, GkEncode.eUTF8);
-}
+// -(void) writeText:(NSString*) text
+// {
+// 	self->writeText(text, GkEncode.eUTF8);
+// }
 
--(void) writeText:(NSString*) text gkEncode:(GkEncode) gkEncode
-{
-	MEncoding encode = UtilSysLibsWrap.convGkEncode2EncodingEncoding(gkEncode);
+// -(void) writeText:(NSString*) text gkEncode:(GkEncode) gkEncode
+// {
+// 	MEncoding encode = UtilSysLibsWrap.convGkEncode2EncodingEncoding(gkEncode);
 
-	self->checkAndOpen();
+// 	self->checkAndOpen();
 
-	if (self->mFile.canWrite())
-	{
-		//if (encode == nil)
-		//{
-		//    encode = GkEncode.UTF8;
-		//}
+// 	if (self->mFile.canWrite())
+// 	{
+// 		//if (encode == nil)
+// 		//{
+// 		//    encode = GkEncode.UTF8;
+// 		//}
 
-		char[] bytes = encode.GetBytes(text);
+// 		char[] bytes = encode.GetBytes(text);
 
-		if (bytes != nil)
-		{
-			try
-			{
-				self->mFileOutputStream.write(bytes, 0, bytes.length);
-			}
-			catch (Exception err)
-			{
+// 		if (bytes != nil)
+// 		{
+// 			try
+// 			{
+// 				self->mFileOutputStream.write(bytes, 0, bytes.length);
+// 			}
+// 			catch (Exception err)
+// 			{
 
-			}
-		}
-	}
-}
+// 			}
+// 		}
+// 	}
+// }
 
--(void) writeByte:(char[]) bytes
-{
-	self->writeByte(bytes, 0, 0);
-}
+// -(void) writeByte:(char[]) bytes
+// {
+// 	self->writeByte(bytes, 0, 0);
+// }
 
--(void) writeByte:(char[]) bytes offset:(int) offset
-{
-	self->writeByte(bytes, offset, 0);
-}
+// -(void) writeByte:(char[]) bytes offset:(int) offset
+// {
+// 	self->writeByte(bytes, offset, 0);
+// }
 
--(void) writeByte:(char[]) bytes offset:(int) offset count:(int) count
-{
-	self->checkAndOpen();
+// -(void) writeByte:(char[]) bytes offset:(int) offset count:(int) count
+// {
+// 	self->checkAndOpen();
 
-	if (self->mFile.canWrite())
-	{
-		if (bytes != nil)
-		{
-			if (count == 0)
-			{
-				count = bytes.length;
-			}
+// 	if (self->mFile.canWrite())
+// 	{
+// 		if (bytes != nil)
+// 		{
+// 			if (count == 0)
+// 			{
+// 				count = bytes.length;
+// 			}
 
-			if (count != 0)
-			{
-				try
-				{
-					self->mFileOutputStream.write(bytes, offset, count);
-				}
-				catch (Exception err)
-				{
+// 			if (count != 0)
+// 			{
+// 				try
+// 				{
+// 					self->mFileOutputStream.write(bytes, offset, count);
+// 				}
+// 				catch (Exception err)
+// 				{
 
-				}
-			}
-		}
-	}
-}
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
--(void) writeLine:(NSString*) text
-{
-	self->writeLine(text, GkEncode.eUTF8);
-}
+// -(void) writeLine:(NSString*) text
+// {
+// 	self->writeLine(text, GkEncode.eUTF8);
+// }
 
--(void) writeLine:(NSString*) text gkEncode:(GkEncode) gkEncode
-{
-	text = text + UtilSysLibsWrap.CR_LF;
-	writeText(text, gkEncode);
-}
+// -(void) writeLine:(NSString*) text gkEncode:(GkEncode) gkEncode
+// {
+// 	text = text + UtilSysLibsWrap.CR_LF;
+// 	writeText(text, gkEncode);
+// }
 
-@end
+// @end
